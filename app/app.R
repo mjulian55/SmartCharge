22166ad0e9d27389068fbef613e5d5765b8a768e
Tom Paschos <tpaschos@bren.ucsb.edu>
2019-02-20 15:30:04 -0800

    server setup

--- app/app.R
+++ app/app.R
@@ -136,38 +136,14 @@ ui <- fluidPage(
 # Air Quality Implications (NOX)
 
 server <- function(input, output) {
-   
-  output$contents <- renderTable({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    
-    req(input$file1)
-    
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
-    tryCatch(
-      {
-        df <- read.csv(input$file1$datapath,
-                       header = input$header,
-                       sep = input$sep,
-                       quote = input$quote)
-      },
-      error = function(e) {
-        # return a safeError if a parsing error occurs
-        stop(safeError(e))
-      }
-    )
-    
-    if(input$disp == "head") {
-      return(head(df))
-    }
-    else {
-      return(df)
-    }
-    
-  })
+
+  
+# This website will make sure any changes in your Input UI tab will be shared amongst all tabs:
+# https://stackoverflow.com/questions/47226158/share-some-ui-between-tabs-with-different-output-on-shiny
+  
+  output$hourly_demand <- renderPlot()
+  
+  
   
 }
 
